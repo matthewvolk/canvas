954c0ccb4726e7a1ff8feeced2e21ec1ff959801
Matthew Volk <volkmattj@gmail.com>
2024-08-25 15:48:35 -0500

    feat: cache us central time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import { Suspense } from "react";
+
+async function Time() {
+  const response = await fetch(
+    "https://timeapi.io/api/time/current/zone?timeZone=America%2FChicago",
+    {
+      headers: {
+        Accept: "application/json",
+      },
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+
+  console.dir(response.body, { depth: null });
+
+  const data = await response.json();
+
+  console.dir(data, { depth: null });
+
+  return <p>{data.dateTime}</p>;
+}
+
 export default function Home() {
-  return null;
+  return (
+    <Suspense
+      fallback={<section className="flex min-h-screen bg-red-600"></section>}
+    >
+      <Time />
+    </Suspense>
+  );
 }
